54c466007dc010f95b4713d2f17638caae514dd0
Ruslan0770 <ruslania@code.edu.az>
2022-04-03 13:05:47 -0700

    csharptask

---
+++ Avg/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+
+namespace Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+
+            //Ruslan();
+
+            //static void Ruslan()
+            //{
+            //    int a = 14;
+            //    int b = 12;
+            //    int c = 13;
+
+            //    if (a > b && a > c)
+            //    {
+            //        Console.WriteLine("a boyukdur");
+
+            //    }
+            //    else if (b > a && b > c)
+            //    {
+            //        Console.WriteLine("b boyukdur");
+            //    }
+            //    else if (c > a && c > b)
+            //    {
+            //        Console.WriteLine("c boyukdur");
+            //    }
+
+            //task2
+
+            #endregion
+
+
+            #region task2
+            //static void avg()
+            //avg();
+            //string a = Console.ReadLine();
+            //int number = int.Parse(a);
+            //string b = Console.ReadLine();
+            //int number1 = int.Parse(b);
+            //string c = Console.ReadLine();
+            //int number2 = int.Parse(c);
+            //string d = Console.ReadLine();
+            //int number3 = int.Parse(d);
+            //string e = Console.ReadLine();
+            //int number4 = int.Parse(e);
+            //double i = (double)(number + number1 + number2 + number3 + number4) / 5;
+            //Console.WriteLine(i);
+
+            #endregion
+
+            #region task3
+
+
+
+
+            //static void afk()
+            //{
+            //afk();
+            //Console.WriteLine("enini daxil edin");
+            //string a = Console.ReadLine();
+            //int cool = int.Parse(a);
+            //Console.WriteLine("uzunlugu daxil edin");
+            //string b = Console.ReadLine();
+            //int cool1 = int.Parse(b);
+            //int p = (cool + cool1) * 2;
+            //Console.WriteLine($"perimetr {p}");
+
+            //int s = cool * cool1;
+            //Console.WriteLine($"sahesi {s}");
+
+
+
+            #endregion
+
+            #region task5
+            //    task5();
+            //}
+            //static void task5()
+            //{
+            //    Console.WriteLine("eded daxil et");
+            //    string a = Console.ReadLine();
+            //    int ball = int.Parse(a);
+            //    int count = 0;
+            //    while (ball > 0)
+            //    {
+            //        ball = ball / 10;
+            //        count++;
+            //    }
+            //    Console.WriteLine(count);
+            #endregion
+
+            #region task6
+            //    task6();
+            //}
+            //static void task6()
+            //{
+            //    Console.WriteLine("Eded daxil edin");
+            //    string a = Console.ReadLine();
+            //    int bash = int.Parse(a);
+            //    while (bash > 1)
+            //    {
+            //        if (bash % 21 == 0)
+            //        {
+            //            Console.WriteLine("bolunur");
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("bolunmur");
+            //            break;
+            //        }
+            //    }
+            #endregion#region task6
+
+            #region task7
+            //    task7();
+            //}
+            //static void task7()
+            //{
+            //    Console.WriteLine("Eded daxil edin");
+            //    string a = Console.ReadLine();
+            //    int ra = int.Parse(a);
+
+            //    if (ra >= 0 && ra <= 100)
+            //    {
+            //        if (ra >= 91 && ra <= 100)
+            //        {
+            //            Console.WriteLine("A");
+            //        }
+            //        else if (ra >= 81 && ra <= 90)
+            //        {
+            //            Console.WriteLine("B");
+            //        }
+            //        else if (ra >= 71 && ra <= 80)
+            //        {
+            //            Console.WriteLine("C");
+
+            //        }
+
+            //        else if (ra >= 61 && ra <= 70)
+            //        {
+
+            //            Console.WriteLine("D");
+            //        }
+            //        else if (ra >= 51 && ra <= 60)
+            //        {
+            //            Console.WriteLine("E");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Kesildiniz");
+            //        }
+            #endregion
+
+            #region task9
+            //    task9();
+            //}
+            //static void task9()
+            //{
+            //    Console.WriteLine("m deyeri");
+            //    string m = Console.ReadLine();
+            //    int sad = int.Parse(m);
+            //    Console.WriteLine("n deyeri");
+            //    string n = Console.ReadLine();
+            //    int sad1 = int.Parse(n);
+            //    int count = 1;
+            //    if (sad > 1 && sad1 > 1)
+            //    {
+            //        for (int i = 1; i <= sad1; i++)
+            //        {
+
+            //            count = count * sad;
+
+            //        }
+            //        Console.WriteLine(count);
+            #endregion
+
+            task4();
+        }
+        static void task4()
+        {
+            Console.WriteLine("Eded daxil edin");
+            string a = Console.ReadLine();
+            int aze = int.Parse(a);
+            int count = 0;
+            if (aze > 1)
+            {
+                for (int i = 1; i <= aze; i++)
+                {
+                    if (aze % i == 0)
+                    {
+                        count++;
+                    }
+                    if (count > 2)
+                    {
+                        Console.WriteLine("murekkebdir");
+                        break;
+                    }
+                    
+
+                    
+                    
+                    
+                    
+                    
+                    
+                        
+                    
+                
+                }
+
+            }
+           
+                        
+                        
+                        
+                           
+                        
+                    
+                    
+                    
+                    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+    
+
+
+        
+
+
+
+        
+        
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+    
+
